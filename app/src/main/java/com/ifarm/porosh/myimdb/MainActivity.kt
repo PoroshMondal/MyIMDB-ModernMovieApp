9a27e453309684ebda258c563db588c11b6be15c
Porosh Mondal <poroshmondal@gmail.com>
2025-08-07 17:53:10 +0600

    design the app bar icons and clickable functionality
    - search view on search icon click

--- app/src/main/java/com/ifarm/porosh/myimdb/MainActivity.kt
+++ app/src/main/java/com/ifarm/porosh/myimdb/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.ifarm.porosh.myimdb
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.Observer
 import com.ifarm.porosh.myimdb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -25,4 +28,60 @@ class MainActivity : AppCompatActivity() {
         //supportActionBar?.title = "My IMDB"
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+
+        val searchItem = menu!!.findItem(R.id.menu_search)
+        val searchView = searchItem.actionView as SearchView?
+        searchView!!.setSubmitButtonEnabled(true)
+
+        searchView.setOnCloseListener {
+            Log.i("mainactivity", "Search data: close");
+            false
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(s: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(s: String?): Boolean {
+                val sQuery = s
+                Log.i("mainactivity","search $sQuery")
+                /*quoteViewModel.searchQuotes(sQuery)
+                    .observe(this@MainActivity, object : Observer<MutableList<Quotes?>?> {
+                        override fun onChanged(quotes: MutableList<Quotes?>?) {
+                            if (quotes != null && !quotes.isEmpty()) {
+                                opViewModel.setSearchQuotes(quotes)
+                                Log.i(
+                                    "mainactivity",
+                                    "Search data: " + quotes.get(0).getQuote() + quotes.get(0)
+                                        .getTitle()
+                                )
+                            }
+                        }
+                    })*/
+                return true
+            }
+        })
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu_search -> {
+                Toast.makeText(this@MainActivity,"Search", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_wishlist -> {
+                Toast.makeText(this@MainActivity,"Wishlist", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_filter -> {
+                Toast.makeText(this@MainActivity,"Filter", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
